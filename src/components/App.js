89339d0a532d948b65e395d72d75d59a3eb86350
VanitLokhande <101892131+VanitLokhande@users.noreply.github.com>
2022-08-12 14:33:26 +0530

    ass

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,23 @@ import '../styles/App.css';
 import { UserProfile } from './UserProfile';
 
 
+import React from 'react'
+import '../styles/App.css';
+import { useState } from "react";
+import { UserProfile } from './UserProfile';
+import { createContext } from "react";
+
+const UserContext = createContext();
+
 const App = () => {
+  const[name , setname]=useState('Newton');
+    const[age ,setage]=useState('3');
 
   return (
     <div id="main">
-      
-        <UserProfile />
-      
+      <UserContext.Provider value={{name,setname,age,setage}}>
+          <UserProfile />
+      </UserContext.Provider>
     </div>
   )
 }
